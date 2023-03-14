52dece6876b3c7b84efe305e4efee3777c034a08
Jan Henning <triton539@outlook.com>
2023-03-14 19:09:46 +0100

    Fix dark/light colors of Hr comp

--- components/Hr.jsx
+++ components/Hr.jsx
@@ -3,5 +3,7 @@
  * @returns {JSX.Element} - Horizontal rule component
  */
 export function Hr() {
-  return <hr className="my-8 border-white/5" />
+  return (
+    <hr className="my-8 dark:border-[rgb(38,38,38)] border-[rgb(229,231,235)]" />
+  )
 }
