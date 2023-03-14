06fdbe4fcdb2703743ea8d017ba438a6aee4d8c9
Jan Henning <triton539@outlook.com>
2023-03-14 11:51:02 +0100

    Add darkMode config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './theme.config.jsx'
+    './theme.config.jsx',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'class',
 }
