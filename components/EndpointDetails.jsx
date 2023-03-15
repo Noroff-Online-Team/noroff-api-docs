8dcbae40291453ceb9bfaac3c8974e79fc0d3d80
Jan Henning <triton539@outlook.com>
2023-03-15 14:09:10 +0100

    Use lookup instead of switch for EndpointDetails

--- components/EndpointDetails.jsx
+++ components/EndpointDetails.jsx
@@ -1,34 +1,24 @@
 /**
  * Endpoint method and path component
- * @param {string} method - HTTP method of the endpoint (GET, POST, PUT, DELETE) (default: GET)
+ * @param {string} method - HTTP method of the endpoint (GET, POST, PUT, PATCH, DELETE) (default: GET)
  * @param {string} path - Path of the endpoint (e.g. /jokes/random)
  * @returns {JSX.Element} Endpoint details component
  */
 export function EndpointDetails({ method = 'GET', path }) {
-  let methodColor = ''
-  switch (method) {
-    case 'GET':
-      methodColor =
-        'ring-green-300 dark:ring-green-500/30 bg-green-400/10 text-green-700 dark:text-green-300'
-      break
-    case 'POST':
-      methodColor =
-        'ring-blue-300 dark:ring-blue-500/30 bg-blue-400/10 text-blue-700 dark:text-blue-300'
-      break
-    case 'PUT':
-      methodColor =
-        'ring-yellow-300 dark:ring-yellow-500/30 bg-yellow-400/10 text-yellow-700 dark:text-yellow-300'
-      break
-    case 'DELETE':
-      methodColor =
-        'ring-red-300 dark:ring-red-500/30 bg-red-400/10 text-red-700 dark:text-red-300'
-      break
+  const methodColors = {
+    GET: 'ring-green-300 dark:ring-green-500/30 bg-green-400/10 text-green-700 dark:text-green-300',
+    POST: 'ring-blue-300 dark:ring-blue-500/30 bg-blue-400/10 text-blue-700 dark:text-blue-300',
+    PUT: 'ring-yellow-300 dark:ring-yellow-500/30 bg-yellow-400/10 text-yellow-700 dark:text-yellow-300',
+    PATCH:
+      'ring-yellow-300 dark:ring-yellow-500/30 bg-yellow-400/10 text-yellow-700 dark:text-yellow-300',
+    DELETE:
+      'ring-red-300 dark:ring-red-500/30 bg-red-400/10 text-red-700 dark:text-red-300',
   }
 
   return (
     <div className="flex items-center gap-x-2 mt-4 font-semibold font-mono ">
       <span
-        className={`leading-6 rounded-lg text-[0.75rem] px-2 py-0.5 ring-1 ring-inset ${methodColor}`}
+        className={`leading-6 rounded-lg text-[0.75rem] px-2 py-0.5 ring-1 ring-inset ${methodColors[method]}`}
       >
         {method}
       </span>
