4486ab1f34298e8a6115ba9e6c8560bea38e3dbd
Jan Henning <triton539@outlook.com>
2023-03-15 12:16:44 +0100

    Create Components barrel file

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { OptionTable } from './Table'
+export { EndpointDetails } from './EndpointDetails'
+export { Properties } from './Properties'
+export { Hr } from './Hr'
+export { Description } from './Description'
