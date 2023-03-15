565e5c8e811d9657d2e7c1a2edf446e983798f59
Jan Henning <triton539@outlook.com>
2023-03-15 11:50:01 +0100

    Update EndpointDetails and Hr components

--- components/EndpointDetails.jsx
+++ components/EndpointDetails.jsx
@@ -5,13 +5,35 @@
  * @returns {JSX.Element} Endpoint details component
  */
 export function EndpointDetails({ method = 'GET', path }) {
+  let methodColor = ''
+  switch (method) {
+    case 'GET':
+      methodColor =
+        'ring-green-300 dark:ring-green-500/30 bg-green-400/10 text-green-700 dark:text-green-300'
+      break
+    case 'POST':
+      methodColor =
+        'ring-blue-300 dark:ring-blue-500/30 bg-blue-400/10 text-blue-700 dark:text-blue-300'
+      break
+    case 'PUT':
+      methodColor =
+        'ring-yellow-300 dark:ring-yellow-500/30 bg-yellow-400/10 text-yellow-700 dark:text-yellow-300'
+      break
+    case 'DELETE':
+      methodColor =
+        'ring-red-300 dark:ring-red-500/30 bg-red-400/10 text-red-700 dark:text-red-300'
+      break
+  }
+
   return (
-    <div className="flex items-center gap-x-3 mt-4">
-      <span className="font-semibold leading-6 rounded-lg px-2 py-1 ring-1 ring-inset ring-slate-300 dark:ring-slate-500/30 bg-slate-400/10 text-slate-700 dark:text-slate-300">
+    <div className="flex items-center gap-x-2 mt-4 font-semibold font-mono">
+      <span
+        className={`leading-7 rounded-lg text-[0.75rem] px-2 ring-1 ring-inset ${methodColor}`}
+      >
         {method}
       </span>
       <span className="h-1 w-1 rounded-full bg-zinc-300 dark:bg-zinc-600"></span>
-      <span className="font-semibold tracking-tight text-lg text-slate-700 dark:text-slate-100">
+      <span className="font-medium tracking-tight text-zinc-700 dark:text-zinc-300">
         {path}
       </span>
     </div>
--- components/Hr.jsx
+++ components/Hr.jsx
@@ -3,7 +3,5 @@
  * @returns {JSX.Element} - Horizontal rule component
  */
 export function Hr() {
-  return (
-    <hr className="my-8 dark:border-[rgb(38,38,38)] border-[rgb(229,231,235)]" />
-  )
+  return <hr className="my-8 dark:border-neutral-800 border-gray-200" />
 }
